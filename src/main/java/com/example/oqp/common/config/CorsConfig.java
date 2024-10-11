44d9b81d13674e81d140a828b9fd9e36d8f487c8
unname <lwer210@naver.com>
2024-10-12 06:23:50 +0900

    Cors 설정 클래스

--- src/main/java/com/example/oqp/common/config/CorsConfig.java
+++ src/main/java/com/example/oqp/common/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.oqp.conmmon.config;
+package com.example.oqp.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
