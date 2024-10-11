1906765242fb0037fb9d606a1d90563f1c611633
unname <lwer210@naver.com>
2024-10-12 06:16:45 +0900

    로그인 request

---
+++ src/main/java/com/example/oqp/user/controller/reqeust/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.example.oqp.user.controller.reqeust;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginRequest {
+
+    private String userId;
+
+    private String password;
+}
