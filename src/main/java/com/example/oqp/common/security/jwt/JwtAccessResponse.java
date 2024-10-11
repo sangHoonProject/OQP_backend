b6d4c47c5db673f6ff4dea7b05f323a8542d4ce7
unname <lwer210@naver.com>
2024-10-12 06:18:04 +0900

    재발급 accesstoken response

---
+++ src/main/java/com/example/oqp/common/security/jwt/JwtAccessResponse.java
@@ -0,0 +1,14 @@
+package com.example.oqp.common.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtAccessResponse {
+    private String accessToken;
+}
