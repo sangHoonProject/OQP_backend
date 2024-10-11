9aa53a78a7328077eaa8e22b2e876c83ec9ebd51
unname <lwer210@naver.com>
2024-10-12 00:48:09 +0900

    UserDetilasService 구현

---
+++ src/main/java/com/example/oqp/conmmon/security/custom/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.oqp.conmmon.security.custom;
+
+import com.example.oqp.user.model.entity.UserEntity;
+import com.example.oqp.user.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("username : {}", username);
+        UserEntity byUserId = userRepository.findByUserId(username);
+        if (byUserId == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new CustomUserDetails(byUserId);
+    }
+}
