59fed220266cdf8af64059db351c2e40bf3cbc58
unname <lwer210@naver.com>
2024-10-11 23:37:00 +0900

    User Repository

---
+++ src/main/java/com/example/oqp/user/model/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.oqp.user.model.repository;
+
+import com.example.oqp.user.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Boolean existsByNickname(String nickname);
+
+    Boolean existsByUserId(String userId);
+}
