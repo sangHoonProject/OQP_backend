3fce40f5c429e9204f6c7ee9008cff524f06aeb7
unname <lwer210@naver.com>
2024-10-11 23:34:09 +0900

    객체 타입 변환 클래스:

---
+++ src/main/java/com/example/oqp/conmmon/converter/Converter.java
@@ -0,0 +1,32 @@
+package com.example.oqp.conmmon.converter;
+
+import com.example.oqp.conmmon.enums.Role;
+import com.example.oqp.user.controller.reqeust.RegisterRequest;
+import com.example.oqp.user.model.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class Converter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserEntity toUserEntity(RegisterRequest request){
+        return UserEntity.builder()
+                .name(request.getName())
+                .nickname(request.getNickname())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .userId(request.getUserId())
+                .email(request.getEmail())
+                .star(0)
+                .postingCount(0)
+                .role(Role.ROLE_USER)
+                .registerAt(LocalDateTime.now())
+                .build();
+    }
+}
+
