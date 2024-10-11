9d1c8f06b21c512cf8c2aa5db1e70a9c6b3e0884
unname <lwer210@naver.com>
2024-10-12 06:22:32 +0900

    Role enum 클래스 EnumType String 지정

--- src/main/java/com/example/oqp/user/model/entity/UserEntity.java
+++ src/main/java/com/example/oqp/user/model/entity/UserEntity.java
@@ -1,13 +1,14 @@
 package com.example.oqp.user.model.entity;
 
-import com.example.oqp.conmmon.enums.Role;
+import com.example.oqp.common.enums.Role;
+import com.example.oqp.content.model.entity.ContentEntity;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserEntity {
 
@@ -36,7 +38,7 @@ public class UserEntity {
     @Schema(description = "사용자 비밀번호", example = "askdfwk")
     private String password;
 
-    @Column(name = "reigster_at")
+    @Column(name = "register_at")
     @Schema(description = "사용자 가입 날짜, 시간")
     private LocalDateTime registerAt;
 
@@ -50,9 +52,18 @@ public class UserEntity {
 
     @Column(nullable = false, name = "role")
     @Schema(description = "사용자 권한(admin의 의미가 없기때문에 ROLE_USER 만 사용)", example = "ROLE_USER")
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Column(nullable = false, name = "name")
     @Schema(description = "사용자 이름", example = "홍길동")
     private String name;
+
+    @Column(nullable = false, name = "email")
+    @Schema(description = "사용자 이메일", example = "hong@gmail.com")
+    private String email;
+
+    @OneToMany(mappedBy = "userId")
+    @Schema(description = "퀴즈 콘텐츠")
+    private List<ContentEntity> content;
 }
