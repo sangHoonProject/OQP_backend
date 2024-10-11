cc1413cd8b1b28d7d6b74db5b1aa32d26eaf1ae1
unname <lwer210@naver.com>
2024-10-12 00:09:21 +0900

    Users table 연동 entity

--- src/main/java/com/example/oqp/user/model/entity/UserEntity.java
+++ src/main/java/com/example/oqp/user/model/entity/UserEntity.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import java.time.LocalDateTime;
 
