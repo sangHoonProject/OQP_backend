bd6d18962ef424afc775425782b82eefb8bae3e7
unname <lwer210@naver.com>
2024-10-11 21:03:51 +0900

    initial setting

---
+++ src/main/java/com/example/oqp/OqpApplication.java
@@ -0,0 +1,13 @@
+package com.example.oqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OqpApplication.class, args);
+	}
+
+}
