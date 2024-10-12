df9515b60bede15a08f050419d0f10b2e1fbf7a9
unname <lwer210@naver.com>
2024-10-13 03:32:45 +0900

    swagger 어노테이션 추가

--- src/main/java/com/example/oqp/content/controller/ContentController.java
+++ src/main/java/com/example/oqp/content/controller/ContentController.java
@@ -3,6 +3,11 @@ package com.example.oqp.content.controller;
 import com.example.oqp.content.pagination.PaginationResponse;
 import com.example.oqp.content.model.entity.ContentEntity;
 import com.example.oqp.content.service.ContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -21,6 +26,12 @@ public class ContentController {
 
     private final ContentService contentService;
 
+    @Operation(summary = "콘텐츠 전체 조회")
+    @ApiResponses(
+            @ApiResponse(responseCode = "200", description = "성공 시 200 반환", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PaginationResponse.class))
+            })
+    )
     @GetMapping("/all")
     public ResponseEntity<PaginationResponse<List<ContentEntity>>> all(@PageableDefault(size = 10, page = 0, sort = "rating", direction = Sort.Direction.DESC) Pageable pageable){
         PaginationResponse<List<ContentEntity>> all = contentService.all(pageable);
