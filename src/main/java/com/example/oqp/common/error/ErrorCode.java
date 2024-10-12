d528ff1fd750df7c16903c065f7f682af3663a2d
unname <lwer210@naver.com>
2024-10-12 23:02:46 +0900

    Custom error code 수정 및 추가

--- src/main/java/com/example/oqp/common/error/ErrorCode.java
+++ src/main/java/com/example/oqp/common/error/ErrorCode.java
@@ -4,11 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    NOT_FOUND_REFRESH_TOKEN(408, "refresh token이 없습니다."),
-    EXPIRED_REFRESH_TOKEN(408, "refresh token 만료"),
 
-    ALREADY_SAVE_ID(409, "이미 사용된 id"),
-    ALREADY_SAVE_NICKNAME(409, "이미 사용된 nickname");
+    USER_NOT_FOUND(407, "사용자를 찾을 수 없음"),
+    USER_NOT_SAME(408, "사용자가 같지 않습니다."),
+
+    NOT_FOUND_REFRESH_TOKEN(408, "token이 없습니다."),
+    EXPIRED_REFRESH_TOKEN(409, "refresh token 만료"),
+
+    ALREADY_SAVE_ID(410, "이미 사용된 user id"),
+    ALREADY_SAVE_NICKNAME(411, "이미 사용된 nickname");
 
     private final int status;
     private final String message;
