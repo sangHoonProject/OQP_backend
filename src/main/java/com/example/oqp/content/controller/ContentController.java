4396954f9fdeb784861958bdaba5c1ccf034ea15
unname <lwer210@naver.com>
2024-10-12 22:21:02 +0900

    Content api 컨트롤러

--- src/main/java/com/example/oqp/content/controller/ContentController.java
+++ src/main/java/com/example/oqp/content/controller/ContentController.java
@@ -1,4 +1,13 @@
 package com.example.oqp.content.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContentController {
+
+    @GetMapping()
+    public String all() {
+
+    }
 }
