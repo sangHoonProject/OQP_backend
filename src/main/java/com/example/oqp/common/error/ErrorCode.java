bca2f077509e6b48b3e2639f6730a3a0ea15048b
unname <lwer210@naver.com>
2024-10-13 00:25:45 +0900

    Error code 변경

--- src/main/java/com/example/oqp/common/error/ErrorCode.java
+++ src/main/java/com/example/oqp/common/error/ErrorCode.java
@@ -8,11 +8,11 @@ public enum ErrorCode {
     USER_NOT_FOUND(407, "사용자를 찾을 수 없음"),
     USER_NOT_SAME(408, "사용자가 같지 않습니다."),
 
-    NOT_FOUND_REFRESH_TOKEN(408, "token이 없습니다."),
-    EXPIRED_REFRESH_TOKEN(409, "refresh token 만료"),
+    NOT_FOUND_TOKEN(409, "token이 없습니다."),
+    EXPIRED_REFRESH_TOKEN(410, "refresh token 만료"),
 
-    ALREADY_SAVE_ID(410, "이미 사용된 user id"),
-    ALREADY_SAVE_NICKNAME(411, "이미 사용된 nickname");
+    ALREADY_SAVE_ID(411, "이미 사용된 user id"),
+    ALREADY_SAVE_NICKNAME(412, "이미 사용된 nickname");
 
     private final int status;
     private final String message;
