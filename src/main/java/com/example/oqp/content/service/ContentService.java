18be8eb190f93869cec5ceff03be95bb34424054
unname <lwer210@naver.com>
2024-10-13 02:53:19 +0900

    content 전체 조회 비즈니스 로직

--- src/main/java/com/example/oqp/content/service/ContentService.java
+++ src/main/java/com/example/oqp/content/service/ContentService.java
@@ -1,4 +1,36 @@
 package com.example.oqp.content.service;
 
+import com.example.oqp.common.pagination.Pagination;
+import com.example.oqp.common.pagination.PaginationResponse;
+import com.example.oqp.content.model.entity.ContentEntity;
+import com.example.oqp.content.model.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ContentService {
+
+    private final ContentRepository contentRepository;
+
+    public PaginationResponse<List<ContentEntity>> all(Pageable pageable) {
+        var list = contentRepository.findAll(pageable);
+        var pagination = Pagination.builder()
+                .size(list.getSize())
+                .page(list.getNumber())
+                .element(list.getNumberOfElements())
+                .totalElement(list.getTotalElements())
+                .totalPage(list.getTotalPages())
+                .build();
+
+        PaginationResponse<List<ContentEntity>> page = PaginationResponse.<List<ContentEntity>>builder()
+                .body(list.getContent())
+                .pagination(pagination)
+                .build();
+
+        return page;
+    }
 }
