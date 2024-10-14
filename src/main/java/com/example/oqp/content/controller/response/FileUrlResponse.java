63621dd302b8222eb7f93ee3e4adf27c94fc2991
unname <lwer210@naver.com>
2024-10-14 22:37:28 +0900

    서버에 저장한 File의 url을 반환

---
+++ src/main/java/com/example/oqp/content/controller/response/FileUrlResponse.java
@@ -0,0 +1,12 @@
+package com.example.oqp.content.controller.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileUrlResponse {
+    private String url;
+}
