faac79a79e9100b0757e7fe5de6ee2d3a0910b9c
unname <lwer210@naver.com>
2024-10-14 22:38:50 +0900

    CustomContentRepository 구현체 메서드 수정 필요

--- src/main/java/com/example/oqp/content/model/repository/CustomContentRepositoryImpl.java
+++ src/main/java/com/example/oqp/content/model/repository/CustomContentRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.example.oqp.content.model.repository;
 
+import com.example.oqp.content.model.entity.ContentEntity;
 import com.example.oqp.content.model.entity.QContentEntity;
 import com.example.oqp.user.model.entity.QUserEntity;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
 public class CustomContentRepositoryImpl implements CustomContentRepository {
 
@@ -15,7 +19,11 @@ public class CustomContentRepositoryImpl implements CustomContentRepository {
     private QContentEntity qContentEntity = QContentEntity.contentEntity;
 
     @Override
-    public List<Object> searchByKeyword(String keyword) {
-        return List.of();
+    public List<ContentEntity> searchByKeyword(String keyword) {
+
+        return queryFactory.selectFrom(qContentEntity)
+                .join(qContentEntity.userId, qUserEntity)
+                .where(qContentEntity.title.contains(keyword).or(qUserEntity.nickname.contains(keyword)))
+                .fetch();
     }
 }
