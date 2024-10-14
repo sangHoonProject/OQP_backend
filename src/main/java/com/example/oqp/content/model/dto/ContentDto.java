852ef30f0e206c44ec1264b21b9f5a2297e720d3
unname <lwer210@naver.com>
2024-10-15 02:17:09 +0900

    Content 응답 dto

---
+++ src/main/java/com/example/oqp/content/model/dto/ContentDto.java
@@ -0,0 +1,30 @@
+package com.example.oqp.content.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContentDto {
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String frontImage;
+
+    private String writer;
+
+    private LocalDateTime createAt;
+
+    private String category;
+
+    private Integer rating;
+}
