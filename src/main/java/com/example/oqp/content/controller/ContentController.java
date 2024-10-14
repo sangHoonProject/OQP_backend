66b7a020b7be202900dbed2ffc70371b96a18d2f
unname <lwer210@naver.com>
2024-10-14 22:37:47 +0900

    upload API 구현

--- src/main/java/com/example/oqp/content/controller/ContentController.java
+++ src/main/java/com/example/oqp/content/controller/ContentController.java
@@ -1,5 +1,7 @@
 package com.example.oqp.content.controller;
 
+import com.example.oqp.common.security.custom.CustomUserDetails;
+import com.example.oqp.content.controller.response.FileUrlResponse;
 import com.example.oqp.content.pagination.PaginationResponse;
 import com.example.oqp.content.model.entity.ContentEntity;
 import com.example.oqp.content.service.ContentService;
@@ -10,16 +12,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -42,14 +45,19 @@ public class ContentController {
         return ResponseEntity.ok(all);
     }
 
-    @Operation(summary = "유저 닉네임 또는 콘텐츠 검색", security = {
+    @Operation(summary = "썸네일 업로드", description = "파일 업로드 후 저장된 url을 리턴해줌", security = {
             @SecurityRequirement(name = "Authorization")
     })
-    @Parameter(name = "keyword", description = "검색할 키워드 입력")
-    @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam(name = "keyword") String keyword){
-        contentService.search(keyword);
-        return null;
+    @Parameter(name = "file", description = "파일 multipart/form-data 로 보내야함")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업로드에 성공하면 200 반환", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = FileUrlResponse.class))
+            })
+    })
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) throws IOException {
+        FileUrlResponse upload = contentService.upload(file);
+        return ResponseEntity.ok(upload);
     }
-    
+
 }
