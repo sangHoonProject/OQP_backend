13c17e75810bbdc664f70f3cd128efb95069c475
unname <lwer210@naver.com>
2024-10-15 01:22:01 +0900

    Content를 추가하기 위한 request

---
+++ src/main/java/com/example/oqp/content/controller/request/ContentAddRequest.java
@@ -0,0 +1,30 @@
+package com.example.oqp.content.controller.request;
+
+import com.example.oqp.content.model.entity.ContentEntity;
+import com.example.oqp.user.model.entity.UserEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContentAddRequest {
+    private String title;
+
+    private String category;
+
+    public static ContentEntity toEntity(ContentAddRequest contentAddRequest, String path, UserEntity user) {
+        return ContentEntity.builder()
+                .title(contentAddRequest.getTitle())
+                .frontImage(path)
+                .writer(user.getNickname())
+                .category(contentAddRequest.getCategory())
+                .createAt(LocalDateTime.now())
+                .rating(0)
+                .userId(user)
+                .build();
+    }
+}
