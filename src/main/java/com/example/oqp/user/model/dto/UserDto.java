2ec6080b05196566b3456e45fbf7d51e279cf92c
unname <lwer210@naver.com>
2024-10-15 17:27:54 +0900

    스네이크 케이스로 네이밍 변경

--- src/main/java/com/example/oqp/user/model/dto/UserDto.java
+++ src/main/java/com/example/oqp/user/model/dto/UserDto.java
@@ -3,6 +3,8 @@ package com.example.oqp.user.model.dto;
 import com.example.oqp.common.enums.Role;
 import com.example.oqp.content.model.dto.ContentDto;
 import com.example.oqp.content.model.entity.ContentEntity;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserDto {
     private Long id;
 
