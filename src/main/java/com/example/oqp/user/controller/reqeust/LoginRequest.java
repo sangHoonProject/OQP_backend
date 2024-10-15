24ebf3e9f3496f4c60cc23bfb6da4c1954213c56
unname <lwer210@naver.com>
2024-10-15 17:45:56 +0900

    userId 정규표현식 적용

--- src/main/java/com/example/oqp/user/controller/reqeust/LoginRequest.java
+++ src/main/java/com/example/oqp/user/controller/reqeust/LoginRequest.java
@@ -2,6 +2,7 @@ package com.example.oqp.user.controller.reqeust;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -13,6 +14,7 @@ import lombok.*;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LoginRequest {
 
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9_-]{4,19}$")
     private String userId;
 
     private String password;
