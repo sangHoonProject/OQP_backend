e3ff439fa60f400011de6b84869e3bca983be7d4
unname <lwer210@naver.com>
2024-10-15 17:25:24 +0900

    Content 전체 조회 dto로 변환 후 return되게 수정

--- src/main/java/com/example/oqp/content/controller/ContentController.java
+++ src/main/java/com/example/oqp/content/controller/ContentController.java
@@ -2,20 +2,15 @@ package com.example.oqp.content.controller;
 
 import com.example.oqp.common.security.custom.CustomUserDetails;
 import com.example.oqp.content.controller.request.ContentAddRequest;
-import com.example.oqp.content.controller.response.FileUrlResponse;
 import com.example.oqp.content.model.dto.ContentDto;
 import com.example.oqp.content.pagination.PaginationResponse;
-import com.example.oqp.content.model.entity.ContentEntity;
 import com.example.oqp.content.service.ContentService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -43,8 +38,8 @@ public class ContentController {
             })
     )
     @GetMapping("/all")
-    public ResponseEntity<PaginationResponse<List<ContentEntity>>> all(@PageableDefault(size = 10, page = 0, sort = "rating", direction = Sort.Direction.DESC) Pageable pageable){
-        PaginationResponse<List<ContentEntity>> all = contentService.all(pageable);
+    public ResponseEntity<PaginationResponse<List<ContentDto>>> all(@PageableDefault(size = 10, page = 0, sort = "rating", direction = Sort.Direction.DESC) Pageable pageable){
+        PaginationResponse<List<ContentDto>> all = contentService.all(pageable);
 
         return ResponseEntity.ok(all);
     }
@@ -67,6 +62,6 @@ public class ContentController {
         return ResponseEntity.ok(upload);
     }
 
-
+    
 
 }
