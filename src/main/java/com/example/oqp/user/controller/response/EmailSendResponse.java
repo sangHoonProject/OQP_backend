977f73193ffd6ec3b9dd99ec1d6d32270b689b1b
unname <lwer210@naver.com>
2024-10-15 19:48:49 +0900

    Email response

---
+++ src/main/java/com/example/oqp/user/controller/response/EmailSendResponse.java
@@ -0,0 +1,12 @@
+package com.example.oqp.user.controller.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailSendResponse {
+    private String message;
+}
