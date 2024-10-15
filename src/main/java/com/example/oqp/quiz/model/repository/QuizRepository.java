f10308a7720252a504a5a73bc37295136d93b513
unname <lwer210@naver.com>
2024-10-15 20:22:19 +0900

    Quiz 리파지토리

---
+++ src/main/java/com/example/oqp/quiz/model/repository/QuizRepository.java
@@ -0,0 +1,7 @@
+package com.example.oqp.quiz.model.repository;
+
+import com.example.oqp.quiz.model.entity.QuizEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizRepository extends JpaRepository<QuizEntity, Long> {
+}
