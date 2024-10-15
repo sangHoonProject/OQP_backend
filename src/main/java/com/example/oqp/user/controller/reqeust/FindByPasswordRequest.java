6ef6cb7ca01a901928fb3738b3e1d8e097daa7da
unname <lwer210@naver.com>
2024-10-15 20:16:07 +0900

    ToString 추가

--- src/main/java/com/example/oqp/user/controller/reqeust/FindByPasswordRequest.java
+++ src/main/java/com/example/oqp/user/controller/reqeust/FindByPasswordRequest.java
@@ -3,15 +3,13 @@ package com.example.oqp.user.controller.reqeust;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class FindByPasswordRequest {
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
