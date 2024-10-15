03e2f944611554ab7fd0b549de8b2af412c23a1f
unname <lwer210@naver.com>
2024-10-15 17:47:37 +0900

    user Id 정규식 추가

--- src/main/java/com/example/oqp/user/controller/reqeust/UserModifyRequest.java
+++ src/main/java/com/example/oqp/user/controller/reqeust/UserModifyRequest.java
@@ -15,6 +15,7 @@ import lombok.*;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserModifyRequest {
 
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9_-]{4,19}$")
     private String userId;
 
     private String password;
