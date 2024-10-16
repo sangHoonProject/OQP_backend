53b426c5a9d0f065671e3322199821877dd35908
unname <lwer210@naver.com>
2024-10-17 00:11:49 +0900

    삭제 완료시 응답을 내려줄 response

---
+++ src/main/java/com/example/oqp/user/controller/response/UserDeleteResponse.java
@@ -0,0 +1,12 @@
+package com.example.oqp.user.controller.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDeleteResponse {
+    private String message;
+}
