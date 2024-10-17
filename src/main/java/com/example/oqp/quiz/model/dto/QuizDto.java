f51ef2aceef50a34e6d6594945519c6ffb90df2d
unname <lwer210@naver.com>
2024-10-17 23:50:34 +0900

    Quiz 응답을 내려줄 dto

---
+++ src/main/java/com/example/oqp/quiz/model/dto/QuizDto.java
@@ -0,0 +1,29 @@
+package com.example.oqp.quiz.model.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class QuizDto {
+
+    private Long id;
+
+    private String problem;
+
+    private String image;
+
+    private String correct;
+
+    private LocalDateTime createAt;
+
+    private Long contentId;
+
+}
