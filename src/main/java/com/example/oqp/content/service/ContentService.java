3aad0317bb72984c70571a00c79465be11cd8aa0
unname <lwer210@naver.com>
2024-10-17 23:49:44 +0900

    Content Quiz 추가 비즈니스 로직 수정 필요

--- src/main/java/com/example/oqp/content/service/ContentService.java
+++ src/main/java/com/example/oqp/content/service/ContentService.java
@@ -4,13 +4,16 @@ import com.example.oqp.common.error.CustomException;
 import com.example.oqp.common.error.ErrorCode;
 import com.example.oqp.common.security.custom.CustomUserDetails;
 import com.example.oqp.content.controller.request.ContentAddRequest;
-import com.example.oqp.content.controller.request.ContentModifyRequest;
 import com.example.oqp.content.model.dto.ContentDto;
 import com.example.oqp.content.model.repository.CustomContentRepository;
 import com.example.oqp.content.pagination.Pagination;
 import com.example.oqp.content.pagination.PaginationResponse;
 import com.example.oqp.content.model.entity.ContentEntity;
 import com.example.oqp.content.model.repository.ContentRepository;
+import com.example.oqp.quiz.controller.request.QuizAddRequest;
+import com.example.oqp.quiz.model.dto.QuizDto;
+import com.example.oqp.quiz.model.entity.QuizEntity;
+import com.example.oqp.quiz.model.repository.QuizRepository;
 import com.example.oqp.user.model.entity.UserEntity;
 import com.example.oqp.user.model.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -34,8 +38,10 @@ public class ContentService {
 
     private final ContentRepository contentRepository;
     private final CustomContentRepository customContentRepository;
+    private final QuizRepository quizRepository;
     private final UserRepository userRepository;
-    private String UPLOAD_PATH = "upload/";
+    private String UPLOAD_CONTENT_PATH = "upload/content/";
+    private String UPLOAD_QUIZ_PATH = "upload/quiz/";
 
     public PaginationResponse<List<ContentDto>> all(Pageable pageable) {
         var list = contentRepository.findAll(pageable);
@@ -69,98 +75,105 @@ public class ContentService {
         return page;
     }
 
-    public ContentDto upload(MultipartFile file, ContentAddRequest request, CustomUserDetails userDetails) throws IOException {
-        File directory = new File(UPLOAD_PATH);
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
+    public ContentDto add(CustomUserDetails customUserDetails, ContentAddRequest contentAddRequest, List<QuizAddRequest> quizAddRequests, MultipartFile contentImage, List<MultipartFile> quizImage) throws IOException {
+        UserEntity user = userRepository.findByUserId(customUserDetails.getUsername());
+        if(contentImage != null){
+            File contentFile = new File(UPLOAD_CONTENT_PATH);
+            if(!contentFile.exists()){
+                contentFile.mkdirs();
+            }
 
-        UUID uuid = UUID.randomUUID();
-        String fileName = uuid.toString() + "." + file.getOriginalFilename();
-        Path path = Paths.get(UPLOAD_PATH + fileName);
-        file.transferTo(path);
+            UUID uuid = UUID.randomUUID();
+            String contentImageName = uuid.toString() + contentImage.getOriginalFilename();
+            Path contentPath = Paths.get(UPLOAD_CONTENT_PATH + contentImageName);
 
-        String userId = userDetails.getUsername();
-        UserEntity user = userRepository.findByUserId(userId);
-        user.setPostingCount(user.getPostingCount() + 1);
-        userRepository.save(user);
-        log.info("user : {}", user);
+            log.info("contentPath : {}", contentPath);
 
-        ContentEntity content = ContentAddRequest.toEntity(request, path.toString(), user);
-        ContentEntity save = contentRepository.save(content);
+            contentImage.transferTo(contentPath);
 
-        return ContentDto.builder()
-                .id(save.getId())
-                .title(save.getTitle())
-                .frontImage(content.getFrontImage())
-                .writer(content.getWriter())
-                .rating(content.getRating())
-                .category(content.getCategory())
-                .createAt(content.getCreateAt())
-                .userId(content.getUserId().getId())
-                .build();
-    }
+            ContentEntity entity = ContentAddRequest.toEntity(contentAddRequest, contentPath.toString(), user);
+            ContentEntity save = contentRepository.save(entity);
 
+            File quizFile = new File(UPLOAD_QUIZ_PATH);
+            if(!quizFile.exists()){
+                quizFile.mkdirs();
+            }
 
-    public ContentDto modify(CustomUserDetails userDetails, ContentModifyRequest request, MultipartFile file) throws IOException {
-        ContentEntity content = contentRepository.findById(request.getId())
-                .orElseThrow(() -> new CustomException(ErrorCode.CONTENT_NOT_FOUND));
+            UUID quizId = UUID.randomUUID();
 
-        String userId = userDetails.getUsername();
-        UserEntity user = userRepository.findByUserId(userId);
-        log.info("user id : {}", user.getId());
+            String fileName = null;
 
-        log.info("content : {}", content.getUserId().getId());
-
-        if (content.getUserId().getId() == user.getId()) {
-
-            Path newPath = null;
-
-            if (file != null) {
-                String oldUrl = content.getFrontImage();
-
-                File directory = new File(UPLOAD_PATH);
-                if (!directory.exists()) {
-                    directory.mkdirs();
+            List<String> quizImageName = new ArrayList<>();
+            for(MultipartFile file : quizImage){
+                fileName = file.getOriginalFilename();
+                if(fileName != null){
+                    fileName += quizId.toString();
+                    quizImageName.add(fileName);
                 }
-                UUID uuid = UUID.randomUUID();
-                String fileName = uuid.toString() + "_" + file.getOriginalFilename();
-                Path path = Paths.get(oldUrl);
-                path.toFile().delete();
-
-                newPath = Paths.get(UPLOAD_PATH + fileName);
-                file.transferTo(newPath);
-                ContentEntity modify = ContentModifyRequest.toEntity(content, request, newPath.toString());
-                ContentEntity save = contentRepository.save(modify);
-
-                return ContentDto.builder()
-                        .id(save.getId())
-                        .title(save.getTitle())
-                        .frontImage(save.getFrontImage())
-                        .writer(save.getWriter())
-                        .createAt(save.getCreateAt())
-                        .category(save.getCategory())
-                        .rating(save.getRating())
-                        .userId(save.getUserId().getId())
-                        .build();
             }
-            else{
-                ContentEntity modify = ContentModifyRequest.toEntity(content, request);
-                ContentEntity save = contentRepository.save(modify);
 
-                return ContentDto.builder()
-                        .id(save.getId())
-                        .title(save.getTitle())
-                        .frontImage(save.getFrontImage())
-                        .writer(save.getWriter())
-                        .createAt(save.getCreateAt())
-                        .category(save.getCategory())
-                        .rating(save.getRating())
-                        .userId(save.getUserId().getId())
-                        .build();
+            List<Path> path = new ArrayList<>();
+            for(String name : quizImageName){
+                if(name != null){
+                    path.add(Paths.get(UPLOAD_QUIZ_PATH + name));
+                }
             }
+
+            List<Path> quizImagePath = new ArrayList<>();
+            for(Path url : path){
+                quizImagePath = quizImage.stream()
+                        .map((image) -> {
+                            try {
+                                image.transferTo(url);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            return url;
+                        })
+                        .collect(Collectors.toList());
+            }
+
+            List<QuizEntity> quiz = new ArrayList<>();
+            for(Path quizUrl : quizImagePath){
+                if(quizUrl != null){
+                    quiz = quizAddRequests.stream()
+                            .map((request) -> {
+                                return QuizAddRequest.toQuizEntity(request, quizUrl.toString(), save);
+                            })
+                            .collect(Collectors.toList());
+                }
+            }
+
+            List<QuizEntity> quizEntities = quizRepository.saveAll(quiz);
+
+            List<QuizDto> quizDtos = quizEntities.stream()
+                    .map((value) -> {
+                        return QuizDto.builder()
+                                .id(value.getId())
+                                .problem(value.getProblem())
+                                .image(value.getImage())
+                                .correct(value.getCorrect())
+                                .createAt(value.getCreateAt())
+                                .contentId(value.getContent().getId())
+                                .build();
+                    }).collect(Collectors.toList());
+
+            ContentDto contentDto = ContentDto.builder()
+                    .id(save.getId())
+                    .title(save.getTitle())
+                    .frontImage(save.getFrontImage())
+                    .writer(save.getWriter())
+                    .createAt(save.getCreateAt())
+                    .category(save.getCategory())
+                    .rating(save.getRating())
+                    .userId(save.getUserId().getId())
+                    .quiz(quizDtos)
+                    .build();
+
+            return contentDto;
+
         }else{
-            throw new CustomException(ErrorCode.USER_NOT_SAME);
+            throw new CustomException(ErrorCode.CONTENT_NOT_FOUND_IMAGE);
         }
     }
 }
