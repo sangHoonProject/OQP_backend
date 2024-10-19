4b5dc40fc3c8f8061e3913ea37b6a8dd3ff060b1
unname <lwer210@naver.com>
2024-10-19 23:01:32 +0900

    삭제 후 응답을 내려줄 Response

---
+++ src/main/java/com/example/oqp/content/controller/response/ContentQuizDeleteResponse.java
@@ -0,0 +1,15 @@
+package com.example.oqp.content.controller.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContentQuizDeleteResponse {
+
+    private String contentDelete;
+
+    private String quizDelete;
+}
