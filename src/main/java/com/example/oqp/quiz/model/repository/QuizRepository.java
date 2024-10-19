54be81dd90be91ba2c6c73a0e81a616e083ea25b
unname <lwer210@naver.com>
2024-10-19 23:01:52 +0900

    deleteByContentId 메서드 정의

--- src/main/java/com/example/oqp/quiz/model/repository/QuizRepository.java
+++ src/main/java/com/example/oqp/quiz/model/repository/QuizRepository.java
@@ -4,4 +4,6 @@ import com.example.oqp.quiz.model.entity.QuizEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuizRepository extends JpaRepository<QuizEntity, Long> {
+
+    void deleteByContentId(Long contentId);
 }
